c25ea70cee7cdbe39f6307adfb19dd8e20d0dec6
Normalizex <104392358+Normalizex@users.noreply.github.com>
2022-05-17 15:58:21 +0100

    Delete dist directory
    Clear dist.

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import Web3 from "web3";
-import ERC20Utils from "./utils/ERC20";
-declare class Ethut {
-    private _web3;
-    erc20: ERC20Utils;
-    constructor(provider: string | Web3);
-}
-export default Ethut;
--- dist/index.js
+++
@@ -1,9 +0,0 @@
-import Web3 from "web3";
-import ERC20Utils from "./utils/ERC20";
-class Ethut {
-    constructor(provider) {
-        this._web3 = typeof provider == 'string' ? new Web3(provider) : provider;
-        this.erc20 = new ERC20Utils(this._web3);
-    }
-}
-export default Ethut;
