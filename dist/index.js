c96ec1fd664e5b7cb5fe3737604ce104ef607e7a
Normalizex <104392358+Normalizex@users.noreply.github.com>
2022-05-17 15:19:04 +0100

    1.0.0-prc.2
    Pre-release 2
    
    Try add web support.

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,9 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var web3_1 = __importDefault(require("web3"));
-var ERC20_1 = __importDefault(require("./utils/ERC20"));
-var Ethut = /** @class */ (function () {
-    function Ethut(provider) {
-        this._web3 = typeof provider == 'string' ? new web3_1.default(provider) : provider;
-        this.erc20 = new ERC20_1.default(this._web3);
+import Web3 from "web3";
+import ERC20Utils from "./utils/ERC20";
+class Ethut {
+    constructor(provider) {
+        this._web3 = typeof provider == 'string' ? new Web3(provider) : provider;
+        this.erc20 = new ERC20Utils(this._web3);
     }
-    return Ethut;
-}());
-exports.default = Ethut;
-//# sourceMappingURL=index.js.map
+}
+export default Ethut;
